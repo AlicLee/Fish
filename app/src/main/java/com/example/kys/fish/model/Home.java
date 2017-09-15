de97def2e3c57a1752782d1e57a387eb7fdc3b6f
zjj <19910066023@qq.com>
2017-09-15 14:48:52 +0800

    我是美女张静静

--- app/src/main/java/com/example/kys/fish/model/Home.java
+++ app/src/main/java/com/example/kys/fish/model/Home.java
@@ -9,19 +9,15 @@ import org.w3c.dom.Comment;
     public class Home {
         private String content;//图片内容
         private int imageId;//图片资源Id
-    private String comment;
 
-        public Home(String content,String comment,int imageId){
+        public Home(String content,int imageId){
             this.content=content;
-            this.comment=comment;
             this.imageId=imageId;
         }
         public String getContent(){
             return content;
         }
-        public String getComment(){
-            return comment;
-        }
+
         public int getImageId(){
             return imageId;
         }
